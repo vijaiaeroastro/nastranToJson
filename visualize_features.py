8ea1ccd1c8ad19a0c38e2044ba2a73376a2700d2
Vijai Kumar Suriyababu <vijai@vijaikumar.in>
2021-09-05 14:42:09 +0200

    include ability to read binary json as well
    Signed-off-by: Vijai Kumar Suriyababu <vijai@vijaikumar.in>

--- visualize_features.py
+++ visualize_features.py
@@ -1,6 +1,19 @@
 import numpy as np
 import polyscope as ps
 import json
+import bson
+
+def isBinary(input_file):
+    f = open(input_file)
+    unicode_error = False
+    try:
+        f.readlines()
+    except UnicodeDecodeError:
+        unicode_error = True
+    if unicode_error:
+        return True
+    else:
+        return False
 
 def vertexFromJson(v_obj):
     vArr = np.empty([len(v_obj), 3], dtype=np.float64)
@@ -21,9 +34,15 @@ def featuresFromJson(f_obj):
     unique_colours = np.random.rand(max_id, 3)
     for f in f_obj:
         chain = f["chain"]
-        for fpair in list(zip(chain, chain[1:])):
-            feature_array.append(fpair)
-            feature_colour.append(unique_colours[f["id"]])
+        if len(chain) == 2:
+            if chain[0] != chain[1]:
+                feature_array.append(chain)
+                feature_colour.append(unique_colours[f["id"]])
+        else:
+            for fpair in list(zip(chain, chain[1:])):
+                if fpair[0] != fpair[1]:
+                    feature_array.append(fpair)
+                    feature_colour.append(unique_colours[f["id"]])
     return np.array(feature_array), np.array(feature_colour)
 
 def trianglesFromJson(f_obj):
@@ -34,9 +53,19 @@ def trianglesFromJson(f_obj):
         fArr[f["id"], 2] = f["v3"]
     return fArr
 
+def readJson(input_file):
+    if isBinary(input_file):
+        f = open(input_file, "rb")
+        bson_data = bson.loads(f.read())
+        return bson_data
+    else:
+        f = open(input_file)
+        json_data = json.load(f)
+        return json_data
+
+
 def jsonToNumpy(input_file):
-    f = open(input_file)
-    data = json.load(f)
+    data = readJson(input_file)
     vertices = vertexFromJson(data["vertices"])
     edges, colours = featuresFromJson(data["feature_curves"])
     triangles = trianglesFromJson(data["triangles"])
@@ -44,7 +73,7 @@ def jsonToNumpy(input_file):
 
 if __name__ == "__main__":
     ps.init()
-    v, t, e, c = jsonToNumpy("/home/vijai.kumar/Code/nastranToJson/cmake-build-release/rotor_shaft.fjson")
+    v, t, e, c = jsonToNumpy("/home/vijai.kumar/Code/nastranToJson/cmake-build-release/binary_test.fjson")
     os_mesh = ps.register_surface_mesh("mesh", v, t)
     ps_net = ps.register_curve_network("Feature Curve", v, e)
     ps_net.add_color_quantity("f_id", c, defined_on='edges')
